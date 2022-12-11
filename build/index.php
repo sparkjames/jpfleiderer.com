7581759af0155e685a8826aa82299ac7be2fae8b
James Pfleiderer <jpfleiderer@gmail.com>
2022-12-11 17:47:38 -0500

    change folder name to build

---
+++ build/index.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+	<link rel='dns-prefetch' href='//fonts.googleapis.com'>
+
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>James Pfleiderer - Senior Web Developer</title>
+
+	<link rel='stylesheet' href='assets/styles/style.css?ver=20221209' type='text/css' media='all'>
+
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26496563-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-26496563-1');
+	</script>
+
+</head>
+
+<body class="home">
+
+
+	<header class="header">
+		<div class="header__inner container">
+
+			<div class="logo">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+					<circle fill="#0C2530" cx="16" cy="16" r="16" />
+					<path fill="#FFF" d="M26.9 10.4c-.2-.7-.6-1.3-1.1-1.8-.5-.5-1.1-1-1.9-1.3-.8-.3-1.7-.5-2.9-.5h-8.2v3.1H19.9c.5 0 .9 0 1.3.1s.8.2 1.1.4c.3.2.6.5.8.8.2.4.3.8.3 1.4 0 .6-.1 1-.3 1.4-.2.4-.5.6-.8.8-.3.2-.7.3-1.1.4-.4.1-.9.1-1.3.1h-3.1v-3.4h-4V20c0 .3-.1.7-.2 1-.1.3-.3.5-.6.7-.3.2-.7.3-1.2.3s-.9-.1-1.1-.2c-.3-.2-.5-.4-.7-.6-.2-.3-.3-.6-.4-.9-.1-.4-.1-.8-.1-1.2v-.8H4.8v1.6c0 1 .2 1.8.5 2.5s.8 1.3 1.3 1.7c.6.4 1.2.8 1.9 1 .7.2 1.4.3 2.2.3 1.3 0 2.4-.2 3.2-.6.8-.4 1.4-.9 1.8-1.5.4-.6.7-1.3.8-2 .1-.7.2-1.4.2-1.9v-.8H21c1.1 0 2.1-.2 2.9-.5.8-.3 1.4-.8 1.9-1.3s.9-1.2 1.1-1.9c.2-.7.3-1.4.3-2.1 0-1-.1-1.7-.3-2.4z" />
+				</svg>
+			</div>
+
+			<h1 class="header__primary-heading">James Pfleiderer <span class="header__sub-heading">Senior Web Developer</span></h1>
+
+		</div>
+	</header>
+
+	<div class="content__outer">
+		<div class="content__inner container">
+
+			<div class="controls">
+				<button class="controls__color-scheme" type="button">
+					<span class="color-scheme-label color-scheme-label--light-mode">Toggle Dark Mode</span>
+					<span class="color-scheme-label color-scheme-label--dark-mode">Toggle Light Mode</span>
+				</button>
+			</div>
+
+			<div class="resume__table">
+
+				<aside class="resume__column sidebar">
+					<section class="sidebar__section">
+						<h2 class="sidebar__heading">&lt;Greetings&gt;</h2>
+						<p>Hi, I'm James! I've been tinkering on the web for as long as I can remember. I'm a
+							self-starting, driven senior web developer who works well indedpendently but also enjoys
+							leading and participating in teams.</p>
+
+						<p>Always looking to hone my skills, currently learning frontend libraries like Angular in my
+							free time. Interested in a position where I can bring my skills in leading development teams
+							and providing expertise in custom WordPress themes and plugins. I thrive in collaborative
+							environments and can help lead development efforts from kickoff to launch to client
+							training.</p>
+
+					</section>
+
+					<section class="sidebar__section" id="skills">
+						<h2 class="sidebar__heading">Skills</h2>
+						<ul class="list list--columns">
+							<li class="list-item">WordPress API</li>
+							<li class="list-item">PHP</li>
+							<li class="list-item">HTML5</li>
+							<li class="list-item">SASS/SCSS</li>
+							<li class="list-item">CSS3</li>
+							<li class="list-item">JavaScript</li>
+							<li class="list-item">jQuery</li>
+							<li class="list-item">mySQL</li>
+							<li class="list-item">SVG</li>
+							<li class="list-item">Git</li>
+							<li class="list-item">Gulp</li>
+							<li class="list-item">Webpack</li>
+							<li class="list-item">508 Compliance</li>
+							<li class="list-item">SEO</li>
+							<li class="list-item">Pagespeed</li>
+							<li class="list-item">Photoshop</li>
+							<li class="list-item">Illustrator</li>
+							<li class="list-item">XD</li>
+							<li class="list-item">SMACSS</li>
+							<li class="list-item">Modular/<wbr>Atomic</li>
+							<li class="list-item">LEMP stack</li>
+						</ul>
+					</section>
+
+					<section class="sidebar__section">
+						<h2 class="sidebar__heading">Fun Facts</h2>
+						<table class="a-wild-table-appears">
+							<tr>
+								<td>Favorite Color:</td>
+								<td>#FF00FF</td>
+							</tr>
+							<tr>
+								<td>Hometown:</td>
+								<td>Orlando, FL</td>
+							</tr>
+							<tr>
+								<td>Favorite Team:</td>
+								<td>UCF</td>
+							</tr>
+							<tr>
+								<td>Fear:</td>
+								<td>Bugs (in code)</td>
+							</tr>
+							<tr>
+								<td>Former Life:</td>
+								<td>Pharmacist</td>
+							</tr>
+						</table>
+					</section>
+
+					<section class="sidebar__section" id="find-me">
+						<h2 class="sidebar__heading">Find Me</h2>
+						<ul class="list list__find">
+							<li class="list-item">
+								<i class="icon icon__home"><img src="assets/images/icon-home.svg" height="16" width="16" alt="Home icon"></i>
+								Washington, DC
+							</li>
+							<li class="list-item">
+								<a href="http://github.com/sparkjames">
+									<i class="icon icon__github"><img src="assets/images/icon-github.svg" height="16" width="16" alt="Github icon"></i>
+									github.com/sparkjames
+								</a>
+							</li>
+							<li class="list-item">
+								<a href="https://codepen.io/sparkjames/">
+									<i class="icon icon__codepen"><img src="assets/images/icon-codepen.svg" height="16" width="16" alt="Codepen icon"></i>
+									codepen.io/sparkjames
+								</a>
+							</li>
+							<li class="list-item">
+								<a href="mailto:jpfleiderer@gmail.com">
+									<i class="icon icon__email"></i>
+									jpfleiderer@gmail.com
+								</a>
+							</li>
+						</ul>
+					</section>
+
+				</aside>
+
+				<div class="resume__column resume__column--right">
+					<section id="education">
+						<h2 class="reume__heading">Education</h2>
+						<p>Bachelor of Digital Media – Interactive Systems, 2009<br>
+							University of Central Florida</p>
+					</section>
+
+					<section id="experience">
+						<h2 class="reume__heading">Experience</h2>
+
+						<p class="list-title">
+							<strong>Edelman, Senior WordPress Developer</strong>
+							<em>(2021 - 2022)</em>
+						</p>
+						<ul class="list list--experience">
+							<!-- <li class="list-item">Provided expertise and led other developers (both in-house and external) on WordPress projects.</li> -->
+						</ul>
+
+						<p class="list-title">
+							<strong>Taoti Creative, Lead WordPress Developer</strong>
+							<em>(2018 - 2021)</em>
+						</p>
+						<ul class="list list--experience">
+							<li class="list-item">In charge of WordPress operations which involved managing other developers and planning
+								sprint/task lists.</li>
+							<li class="list-item">Provided expertise and led other developers (both in-house and external) on WordPress
+								projects.</li>
+							<li class="list-item">Created WordPress sites, as a solo developer and as a team, from in-house Photoshop and
+								XD designs.</li>
+							<li class="list-item">Planned and managed site functionality and architecture as projects moved from sales, to
+								planning, to the design process.</li>
+							<li class="list-item">Created "scope of work" and "build specification" documents to help define our projects'
+								development deliverables and timelines.</li>
+							<li class="list-item">Implemented custom WordPress boilerplate to from which we began all WordPress projects.
+							</li>
+							<li class="list-item">Built WordPress plugins to connect with the APIs like HubSpot, Google Maps, and
+								Dribbble.</li>
+							<li class="list-item">Extensive work with PHP, HTML, SCSS, Javascript, Twig, and Gulp.</li>
+							<li class="list-item">All sites responsive and cross-browser compatible.</li>
+						</ul>
+
+						<p class="list-title">
+							<strong>Knucklepuck, Senior Web Developer</strong>
+							<em>(2016 - 2018)</em>
+						</p>
+						<ul class="list list--experience">
+							<li class="list-item">Planned/created WordPress sites, as a solo developer and as a team, from wireframes to
+								development.</li>
+							<li class="list-item">Communicated directly with clients to provide expertise on site functionality and
+								content management.</li>
+							<li class="list-item">Created an extentible module system to build WordPress templates.</li>
+							<li class="list-item">Extensive work with PHP, HTML, SCSS, Javascript, Twig, and Gulp.</li>
+							<li class="list-item">All sites responsive and cross-browser compatible.</li>
+							<li class="list-item">Worked with clients to plan redesigns of their websites.</li>
+							<li class="list-item">Used modern pagespeed methods to optimize websites for high performance scores on Google
+								Lighthouse audits.</li>
+							<li class="list-item">Collaborated with SEO and Paid Media teams to plan websites that are highly visible to
+								search engines.</li>
+						</ul>
+
+						<p class="list-title">
+							<strong>SPARK Experience, Senior Web Developer</strong>
+							<em>(2012 - 2016)</em>
+						</p>
+						<ul class="list list--experience">
+							<li class="list-item">Planned/created WordPress sites with a team of developers.</li>
+							<li class="list-item">Worked directly with clients to identify needs/goals and plan solutions.</li>
+							<li class="list-item">Used modular workflow to accelerate development process.</li>
+							<li class="list-item">Used WordPress API for custom themes, plugins, and admin areas.</li>
+							<li class="list-item">Extensive work with PHP, HTML, SCSS, and Javascript/jQuery.</li>
+							<li class="list-item">All sites responsive and cross-browser compatible.</li>
+							<li class="list-item">Managed and maintained projects with Git and Grunt/Gulp.</li>
+							<li class="list-item">Planned projects alongside UX research, content, and design teams.</li>
+						</ul>
+
+						<p><strong>pro2go Designs, Co-Founder</strong> <em>(2009 - 2012)</em></p>
+						<!-- <ul class="list">
+							<li class="list-item">Met with clients for project milestone updates.</li>
+							<li class="list-item">Created websites with WordPress API or a custom PHP/mySQL CMS.</li>
+							<li class="list-item">All websites created with PHP, HTML, CSS, and Javascript/jQuery.</li>
+							<li class="list-item">Designed websites in Photoshop and Illustrator.</li>
+							<li class="list-item">A variety of design work – from full websites to Facebook covers.</li>
+						</ul> -->
+
+						<p><strong>Planet Digital, PHP Developer</strong> <em>(2010)</em></p>
+						<p><strong>UCF SDES Webmaster</strong> <em>(2009)</em></p>
+						<p><strong>Time and Gems Webmaster</strong> <em>(2008 - 2009)</em></p>
+						<p><strong>Project Spot Designer</strong> <em>(2007 - 2008)</em></p>
+
+					</section>
+
+					<section id="recent">
+						<h2 class="reume__heading">Recent Websites</h2>
+
+						<ul class="recent-list">
+
+							<li class="list-item">
+								<strong><a href="https://www.rootsandshoots.org/">Jane Goodall - Roots and
+										Shoots</a></strong>, <em>(2019 / in progress)</em>
+								<p>It's BuddyPress with a many custom additions, including how users create their own
+									content and a map of all of their projects grouped by ZIP or city.</p>
+							</li>
+
+							<li class="list-item">
+								<strong><a href="https://hstreet.org/">H Street Main Street</a></strong>,
+								<em>(2018)</em>
+								<p>There are many page speed optimizations on this site, like lazy loading, image
+									compression, and reducing render-blocking stylesheets and scripts. The client is
+									able to add information for local businesses which are then automatically
+									categorized and mapped to the different sections.</p>
+							</li>
+
+							<li class="list-item">
+								<strong><a href="https://www.taoti.com/">Taoti Creative</a></strong>, <em>(2018)</em>
+								<p>In addition to page speed optimizations, there are some neat front-end features here
+									like the 90&deg; main nav and images that are positioned/sized with linear
+									equations.</p>
+							</li>
+
+							<li class="list-item">
+								<strong><a href="https://www.knucklepuckmedia.com/">Knucklepuck</a></strong>,
+								<em>(2018)</em>
+								<p>This is built with a custom, modular WordPress theme. I managed to achieve a <a href="#modal" class="modal-link" data-screenshot="assets/images/2018-03-15_kp-audit.png">95 score on the
+										Lighthouse performance audit</a> by coding a way for WP to queue critical CSS
+									separate from non-critical CSS.</p>
+							</li>
+
+							<li class="list-item">
+								<strong><a href="https://www.danielmrosenberg.com/">Daniel M. Rosenberg</a></strong>,
+								<em>(2017)</em><br>
+								<strong><a href="https://cbcsettlementfunding.com/">CBC Settlement Funding</a></strong>,
+								<em>(2017)</em>
+								<p>Both of these are great examples of a modular WordPress theme. They are built on my
+									extentible system where custom content modules can be added in for whatever the
+									website's needs are.</p>
+							</li>
+
+							<li class="list-item">
+							</li>
+
+						</ul><!-- END .recent-list -->
+
+					</section><!-- END #recent -->
+
+					<section id="awards">
+						<h2 class="icon icon__awards reume__heading">Awards</h2>
+
+						<p class="list-title"><strong>Communicator Awards</strong></p>
+						<ul class="list list--columns list--awards">
+							<li class="list-item"><a href="https://www.ustelecom.org/">USTelecom</a>, <em>Website Redesign 2019</em></li>
+							<li class="list-item"><a href="https://nwlc.org/">NWLC.org</a>, <em>2016</em></li>
+							<li class="list-item"><a href="https://kidneynation.org/">KidneyNation.org</a>, <em>2016</em></li>
+							<li class="list-item"><a href="https://betterworldcampaign.org/">BetterWorldCampaign.org</a>, <em>2016</em>
+							</li>
+							<li class="list-item"><a href="https://www.mejditours.com/">Mejditours.com</a>, <em>2014</em></li>
+							<li class="list-item"><a href="https://rasikarestaurant.com/">RasikaRestaurant.com</a>, <em>2013</em></li>
+							<li class="list-item"><a href="https://jewishfoodexperience.com/">JewishFoodExperience.com</a>, <em>2013</em>
+							</li>
+						</ul>
+
+						<p class="list-title"><strong>W3 Awards</strong></p>
+						<ul class="list list--columns list--awards">
+							<li class="list-item"><a href="https://awr.org/">AWR.org</a>, <em>2015</em></li>
+						</ul>
+					</section>
+
+
+				</div>
+
+			</div><!-- END .resume__table -->
+
+		</div><!-- END .content__inner -->
+	</div><!-- END .wrapper__outer -->
+
+	<div id="modal">
+		<a id="modal-inner" href="#0">
+			<img id="modal-image" src="assets/images/placeholder.gif" alt="">
+		</a>
+	</div>
+
+	<script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
