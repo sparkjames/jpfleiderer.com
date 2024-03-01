227ea52c47816f799433b443136eba5448914829
sparkjames <jpfleiderer@gmail.com>
2024-03-01 10:19:57 -0500

    update resume

--- build/index.php
+++ build/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en-US">
-
 <head>
 	<link rel='dns-prefetch' href='//fonts.googleapis.com'>
 
@@ -10,35 +9,23 @@
 
 	<title>James Pfleiderer - Senior Web Developer</title>
 
-	<noscript>
-		<link rel='stylesheet' href='assets/styles/style.css?ver=20230110' type='text/css' media='all'>
-	</noscript>
-	<link rel='stylesheet' href='assets/styles/style.css?ver=20230110' type='text/css' media='print' onload="this.media='all'; this.onload=null;">
-	<style>
-		body {
-			opacity: 0
-		}
+	<style><?php echo file_get_contents('assets/styles/style-critical.css'); ?></style>
 
-		;
-	</style>
+	<?php
+	$css_version = filemtime('assets/styles/style-critical.css');
+	?>
+	<noscript>
+		<link rel='stylesheet' href='assets/styles/style.css?ver=<?php echo $css_version; ?>' type='text/css' media='all'>
+	</noscript>
+	<link rel='stylesheet' href='assets/styles/style.css?ver=<?php echo $css_version; ?>' type='text/css' media='print' onload="this.media='all'; this.onload=null;">
 
 	<!-- Google tag (gtag.js) -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26496563-1"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-
-		gtag('config', 'UA-26496563-1');
-	</script>
+	<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','UA-26496563-1');</script>
 
 </head>
 
 <body class="home">
-
 
 	<header class="header">
 		<div class="header-inner container">
@@ -75,33 +62,36 @@
 			<div class="resume-table">
 
 				<aside class="resume-column sidebar">
-					<section class="sidebar-section">
-						<h2 class="sidebar-heading">&lt;Greetings&gt;</h2>
-						<p>Hi, I'm James! I've been tinkering on the web for as long as I can remember. I'm a
-							self-starting, driven senior web developer who works well independently but also enjoys
-							leading and participating in teams.</p>
 
-						<p>Always looking to hone my skills, currently learning frontend libraries like Angular in my
-							free time. Interested in a position where I can bring my skills in leading development teams
-							and providing expertise in custom WordPress themes and plugins. I thrive in collaborative
-							environments and can help lead development efforts from kickoff to launch to client
-							training.</p>
+					<section class="sidebar-section">
+
+						<h2 class="sidebar-heading">&lt;Greetings&gt;</h2>
+
+						<p>Hi, I'm James! I've been tinkering on the web for as long as I can remember. I'm a self-starting, driven senior full-stack developer who works well independently but also enjoys leading and participating in teams.</p>
+
+						<p>Always looking to hone my skills, currently working on more React and Angular projects in my free time. Interested in a position where I can bring my skills in leading development teams, providing guidance to clients, and using my expertise in custom WordPress development. I thrive in collaborative environments and can help lead development efforts from kickoff to launch to client training.</p>
 
 					</section>
 
 					<section class="sidebar-section" id="skills">
+
 						<h2 class="sidebar-heading">Skills</h2>
+
 						<ul class="list list--columns">
 							<li class="list-item">WordPress API</li>
 							<li class="list-item">PHP</li>
-							<li class="list-item">HTML5</li>
-							<li class="list-item">SASS/SCSS</li>
 							<li class="list-item">JavaScript</li>
+							<li class="list-item">React &amp; <wbr>JSX</li>
 							<li class="list-item">ES6 &amp; <wbr>jQuery</li>
+							<li class="list-item">Angular</li>
+							<li class="list-item">Typescript</li>
+							<li class="list-item">SASS/SCSS</li>
+							<li class="list-item">HTML5</li>
 							<li class="list-item">mySQL</li>
 							<li class="list-item">SVG</li>
 							<li class="list-item">Git</li>
 							<li class="list-item">Gulp/<wbr>Parcel</li>
+							<li class="list-item">OpenAI/ChatGPT</li>
 							<li class="list-item">Accesibility</li>
 							<li class="list-item">i18n</li>
 							<li class="list-item">SEO</li>
@@ -111,12 +101,15 @@
 							<li class="list-item">Figma/<wbr>Sketch</li>
 							<li class="list-item">SMACSS/<wbr>BEM</li>
 							<li class="list-item">Modular/<wbr>Atomic</li>
-							<li class="list-item">LEMP stack</li>
+							<li class="list-item">LAMP/LEMP stack</li>
 						</ul>
+
 					</section>
 
 					<section class="sidebar-section">
+
 						<h2 class="sidebar-heading">Fun Facts</h2>
+
 						<table class="a-wild-table-appears">
 							<tr>
 								<td>Favorite Color:</td>
@@ -139,10 +132,13 @@
 								<td>Pharmacist</td>
 							</tr>
 						</table>
+
 					</section>
 
 					<section class="sidebar-section sidebar-section--find-me" id="find-me">
+
 						<h2 class="sidebar-heading">Find Me</h2>
+
 						<ul class="list list--find">
 							<li class="list-item">
 								<i class="icon icon--home"><img src="assets/images/icon-home.svg" height="16" width="16" alt="Home icon"></i>
@@ -179,8 +175,34 @@
 							University of Central Florida</p>
 					</section>
 
+					<!-- <section id="current" class="resume-section">
+
+						<h2 class="resume-heading">Current Employment</h2>
+
+						<p class="list-title">
+							<strong><a href="https://nclud.com" target="_blank">nclud</a>, Senior Full-stack Developer</strong>
+							<em>(2023 - 2024)</em>
+						</p>
+
+					</section> -->
+
 					<section id="experience" class="resume-section">
 						<h2 class="resume-heading">Experience</h2>
+
+						<p class="list-title">
+							<strong>nclud, Senior Full-stack Developer</strong>
+							<em>(2023-2024)</em>
+						</p>
+						<ul class="list list--experience">
+							<li class="list-item">Set up deployment processes (CI/CD) with Git Actions for our websites and projects hosted on AWS Lightsail.</li>
+							<li class="list-item">Develop custom Gutenberg blocks using both React and Advanced Custom Fields.</li>
+							<li class="list-item">Build fully custom WordPress websites with full multi-language features (i18n).</li>
+							<li class="list-item">Worked directly with clients to manage work requests and review project progress.</li>
+							<li class="list-item">Plan and manage tasks for myself and other developers on the team.</li>
+							<li class="list-item">Provided expertise and guidance for other in-house developers on WordPress projects.</li>
+							<li class="list-item">Extensive work with PHP, HTML, SCSS, Javascript, Gulp, and Parcel.</li>
+							<li class="list-item">All sites responsive and cross-browser compatible.</li>
+						</ul>
 
 						<p class="list-title">
 							<strong>Edelman, Senior WordPress Developer</strong>
@@ -188,11 +210,15 @@
 						</p>
 						<ul class="list list--experience">
 							<li class="list-item">Prepared and mapped out site features and architecture based on client needs.</li>
-							<li class="list-item">Implemented custom WordPress boilerplate to from which we began all WordPress projects, using Parcel for ease of use by other developers.</li>
-							<li class="list-item">Built WordPress sites from scratch as a solo developer and as a team, using Sketch and Figma designs from in-house designers.</li>
-							<li class="list-item">Built WordPress themes and plugins to connect with different APIs from weather and stock prices to custom content APIs provided by the client.</li>
+							<li class="list-item">Implemented custom WordPress boilerplate to from which we began all WordPress
+								projects, using Parcel for ease of use by other developers.</li>
+							<li class="list-item">Built WordPress sites from scratch as a solo developer and as a team, using Sketch
+								and Figma designs from in-house designers.</li>
+							<li class="list-item">Built WordPress themes and plugins to connect with different APIs from weather and
+								stock prices to custom content APIs provided by the client.</li>
 							<li class="list-item">Built custom API for WordPress to serve content to third party websites.</li>
-							<li class="list-item">Provided expertise and guidance for other in-house developers on WordPress projects.</li>
+							<li class="list-item">Provided expertise and guidance for other in-house developers on WordPress projects.
+							</li>
 							<li class="list-item">Extensive work with PHP, HTML, SCSS, Javascript, Gulp, and Parcel.</li>
 							<li class="list-item">All sites responsive and cross-browser compatible.</li>
 						</ul>
@@ -202,13 +228,20 @@
 							<em>(2018 - 2021)</em>
 						</p>
 						<ul class="list list--experience">
-							<li class="list-item">In charge of WordPress operations which involved managing other developers and planning sprint/task lists.</li>
-							<li class="list-item">Provided expertise and led other developers (both in-house and external) on WordPress projects.</li>
-							<li class="list-item">Created WordPress sites, as a solo developer and as a team, from in-house Photoshop and XD designs.</li>
-							<li class="list-item">Planned and managed site functionality and architecture as projects moved from sales, to planning, to the design process.</li>
-							<li class="list-item">Created "scope of work" and "build specification" documents to help define our projects' development deliverables and timelines.</li>
-							<li class="list-item">Implemented custom WordPress boilerplate to from which we began all WordPress projects.</li>
-							<li class="list-item">Built WordPress plugins to connect with APIs like HubSpot, Google Maps, and Dribbble.</li>
+							<li class="list-item">In charge of WordPress operations which involved managing other developers and
+								planning sprint/task lists.</li>
+							<li class="list-item">Provided expertise and led other developers (both in-house and external) on
+								WordPress projects.</li>
+							<li class="list-item">Created WordPress sites, as a solo developer and as a team, from in-house Photoshop
+								and XD designs.</li>
+							<li class="list-item">Planned and managed site functionality and architecture as projects moved from
+								sales, to planning, to the design process.</li>
+							<li class="list-item">Created "scope of work" and "build specification" documents to help define our
+								projects' development deliverables and timelines.</li>
+							<li class="list-item">Implemented custom WordPress boilerplate to from which we began all WordPress
+								projects.</li>
+							<li class="list-item">Built WordPress plugins to connect with APIs like HubSpot, Google Maps, and
+								Dribbble.</li>
 							<li class="list-item">Extensive work with PHP, HTML, SCSS, Javascript, Twig, and Gulp.</li>
 							<li class="list-item">All sites responsive and cross-browser compatible.</li>
 						</ul>
@@ -218,7 +251,8 @@
 							<em>(2016 - 2018)</em>
 						</p>
 						<ul class="list list--experience">
-							<li class="list-item">Planned/created WordPress sites, as a solo developer and as a team, from wireframes to
+							<li class="list-item">Planned/created WordPress sites, as a solo developer and as a team, from wireframes
+								to
 								development.</li>
 							<li class="list-item">Communicated directly with clients to provide expertise on site functionality and
 								content management.</li>
@@ -226,9 +260,11 @@
 							<li class="list-item">Extensive work with PHP, HTML, SCSS, Javascript, Twig, and Gulp.</li>
 							<li class="list-item">All sites responsive and cross-browser compatible.</li>
 							<li class="list-item">Worked with clients to plan redesigns of their websites.</li>
-							<li class="list-item">Used modern pagespeed methods to optimize websites for high performance scores on Google
+							<li class="list-item">Used modern pagespeed methods to optimize websites for high performance scores on
+								Google
 								Lighthouse audits.</li>
-							<li class="list-item">Collaborated with SEO and Paid Media teams to plan websites that are highly visible to
+							<li class="list-item">Collaborated with SEO and Paid Media teams to plan websites that are highly visible
+								to
 								search engines.</li>
 						</ul>
 
@@ -269,12 +305,27 @@
 						<ul class="list list--recent">
 
 							<li class="list-item">
-								<strong><a href="https://beinillinois.org/" rel="nofollow noopener">Be In Illinois</a></strong>, <em>(2022)</em>
+								<strong><a href="https://www.spendhq.com/" rel="nofollow noopener">SpendHQ</a></strong>,
+								<em>(2023)</em>
+								<p>The website features many custom Gutenberg blocks for an easy admin experience. The loading speed is very fast featuring many lazy-loaded assets and critical/non-critical CSS. It is also fully ready for translation into other languages (i18n).</p>
+							</li>
+
+							<li class="list-item">
+								<strong><a href="https://babytracker5000.com/" rel="nofollow noopener">BabyTracker 5000</a></strong>,
+								<em>(2023)</em>
+								<p>A newborn feeding tracker web app built in Angular. It uses Firebase Auth and Realtime Database APIs to store data, or if you use the app in offline mode it uses your device's localStorage instead. View the <a rel="nofollow noopener" href="https://github.com/sparkjames/babytracker5000">GitHub repo</a>.
+								</p>
+							</li>
+
+							<li class=" list-item">
+								<strong><a href="https://beinillinois.org/" rel="nofollow noopener">Be In Illinois</a></strong>,
+								<em>(2022)</em>
 								<p>Using a custom method for WP to separately queue critical and non-critical CSS, this website is build to load fast even with the high-res images at the top. Modular page sections and Advanced Custom Fields make it easy to add new pages.</p>
 							</li>
 
 							<li class="list-item">
-								<strong><a href="https://www.preludecapital.com/" rel="nofollow noopener">Prelude Capital</a></strong>, <em>(2021)</em>
+								<strong><a href="https://www.preludecapital.com/" rel="nofollow noopener">Prelude Capital</a></strong>,
+								<em>(2021)</em>
 								<p>Page speed was a big emphasis for this project, <a href="#modal" class="modal-link" data-screenshot="assets/images/2022-12-12_prelude-audit.png" data-alt="We achieved a 97 for performance on Prelude Capital's website.">we achieved a 97 for performance</a> despite the videos and high res images on the home page. This site will also feature multiple languages using the Polylang plugin.</p>
 							</li>
 
@@ -298,10 +349,9 @@
 							</li> -->
 
 							<li class="list-item">
-								<strong><a href="https://www.taoti.com/" rel="nofollow noopener">Taoti Creative</a></strong>, <em>(2018)</em>
-								<p>In addition to page speed optimizations, there are some neat front-end features here
-									like the 90&deg; main nav and images that are positioned/sized with linear
-									equations.</p>
+								<strong><a href="https://www.taoti.com/" rel="nofollow noopener">Taoti Creative</a></strong>,
+								<em>(2018)</em>
+								<p>In addition to page speed optimizations, there are some neat front-end features here like the 90&deg; main nav and images that are positioned/sized with linear equations.</p>
 							</li>
 
 							<!-- <li class="list-item">
@@ -331,14 +381,17 @@
 
 						<p class="list-title"><strong>Communicator Awards</strong></p>
 						<ul class="list list--columns list--awards">
-							<li class="list-item"><a href="https://www.ustelecom.org/">USTelecom</a>, <em>Website Redesign 2019</em></li>
+							<li class="list-item"><a href="https://www.ustelecom.org/">USTelecom</a>, <em>Website Redesign 2019</em>
+							</li>
 							<li class="list-item"><a href="https://nwlc.org/">NWLC.org</a>, <em>2016</em></li>
 							<li class="list-item"><a href="https://kidneynation.org/">KidneyNation.org</a>, <em>2016</em></li>
-							<li class="list-item"><a href="https://betterworldcampaign.org/">BetterWorldCampaign.org</a>, <em>2016</em>
+							<li class="list-item"><a href="https://betterworldcampaign.org/">BetterWorldCampaign.org</a>,
+								<em>2016</em>
 							</li>
 							<li class="list-item"><a href="https://www.mejditours.com/">Mejditours.com</a>, <em>2014</em></li>
 							<li class="list-item"><a href="https://rasikarestaurant.com/">RasikaRestaurant.com</a>, <em>2013</em></li>
-							<li class="list-item"><a href="https://jewishfoodexperience.com/">JewishFoodExperience.com</a>, <em>2013</em>
+							<li class="list-item"><a href="https://jewishfoodexperience.com/">JewishFoodExperience.com</a>,
+								<em>2013</em>
 							</li>
 						</ul>
 
@@ -362,14 +415,7 @@
 		</a>
 	</div>
 
-	<script src="assets/js/scripts.js?ver=20230110" async></script>
-	<style>
-		body {
-			opacity: 1
-		}
-
-		;
-	</style>
+	<script src="assets/js/scripts.js?ver=<?php echo filemtime('assets/js/scripts.js'); ?>" async></script>
 </body>
 
 </html>
